fdec8da672a8ddc117bcdec936c8f8da414a93f5
Steve Mayne <steve.mayne@gmail.com>
2026-02-23 07:58:16 +0000

    tweak fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import 'dseg/css/dseg.css'
 import './index.css'
 import App from './App.tsx'
 
+// Pre-load DSEG fonts so they're available for canvas rendering
+Promise.all([
+  document.fonts.load("bold 16px 'DSEG7-Classic'"),
+  document.fonts.load("bold 16px 'DSEG7-Modern'"),
+  document.fonts.load("bold 16px 'DSEG14-Classic'"),
+  document.fonts.load("bold 16px 'DSEG14-Modern'"),
+]).catch(() => {});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
